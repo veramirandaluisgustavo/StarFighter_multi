f4172e2076293e56dc412a51d1e3d4406ec77bb1
Luis Gustavo Vera Miranda <vera.miranda.luis.gustavo@gmail.com>
2022-06-02 15:58:00 -0400

    save

---
+++ Source/StarFighter/murosaleatorios.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "murosaleatorios.h"
+
+// Sets default values
+Amurosaleatorios::Amurosaleatorios()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Amurosaleatorios::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Amurosaleatorios::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
